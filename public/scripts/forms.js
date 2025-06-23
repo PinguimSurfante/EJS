be3b5c0fdc27f0dc1ea258d16e8d9a4c4e491361
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2025-06-22 22:57:50 -0300

    fix

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -1,34 +1,33 @@
-        $(".forms").on("submit", function (e) {
+$(".forms").on("submit", function (e) {
+  e.preventDefault();
 
-          e.preventDefault();
+  console.log("Fetch sendo chamado");
 
-          console.log("Fetch sendo chamado")
+  const id = $(this).data("doc");
+  const endpoint = `/edit/${id}`;
 
-          const id = $(this).data("doc");
-          const endpoint = `/edit/${id}`;
+  const title = $(this).find("input[name='title']").val();
+  const conteudo = $(this).find("textarea[name='conteudo']").val();
 
-          const title = $(this).find("input[name='title']").val();
-          const conteudo = $(this).find("textarea[name='conteudo']").val();
+  const data = {
+    title: title,
+    conteudo: conteudo,
+  };
 
-          const data = {
-          title: title,
-          conteudo: conteudo
-          };
-          
-          fetch(endpoint, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-          })
-            .then((res) => {
-              if (!res.ok) throw new Error("Erro ao Atualizar");
-              return res.json();
-            })
-            .then((result) => {
-            console.log("Atualizado com sucesso");
-            window.location.href = `/blog/${result.post.id}`; 
-          })
-            .catch((e) => console.error("Erro ao atualizar:", e));
-        });
+  fetch(endpoint, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Erro ao Atualizar");
+      return res.json();
+    })
+    .then((result) => {
+      console.log("Atualizado com sucesso");
+      window.location.href = `/blog/${result.post.id}`;
+    })
+    .catch((e) => console.error("Erro ao atualizar:", e));
+});
