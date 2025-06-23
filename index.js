5bf5e03b13aeefff36a3231c9e0fc1f9c99c99dd
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2025-06-22 21:14:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+import express from "express"
+
+const app = express();
+const port = 3000;
+
+let info = []
+let numero = 1
+
+app.use(express.static("public"))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { info })
+})
+
+app.get("/add", (req, res) => {
+    res.render("adicionar.ejs")
+})
+
+
+
+app.get("/about", (req,res) => {
+    res.render("about.ejs")
+})
+
+app.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const post = info.find((p) => p.id == id);
+
+  if (!post) {
+    return res.status(404).send("Post não encontrado");
+  }
+
+  res.render("editar.ejs", { post });
+});
+
+app.get("/blog/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = info.find((p) => p.id === id);
+
+  if (!post) {
+    return res.status(404).send("Post não encontrado");
+  }
+
+  res.render("page.ejs", { post });
+});
+
+
+
+app.post("/submit", (req, res) => {
+    console.log(req.body)
+    const { title, conteudo } = req.body;
+    info.push({
+        id: numero++,
+        title: title,
+        conteudo: conteudo
+    });
+
+
+    res.redirect("/")
+})
+
+app.delete("/remove/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = info.findIndex(p => p.id === id);
+
+
+    if (post !== -1) {
+        info.splice(post, 1);
+        res.json({ redirect: "/" });
+    } else {
+        res.status(404).json({ error: "Post não encontrado" });
+    }
+
+})
+
+app.put("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = info.find((p) => p.id === id)
+    const { title, conteudo } = req.body
+
+    if(!post) {
+        return res.status(400).send("Id não encontrado")
+    }
+
+    post.title = title
+    post.conteudo = conteudo
+
+    console.log(info)
+
+    res.json({ message: "Post atualizado com sucesso", post });
+})
+
+
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
---
+++ public/scripts/forms.js
@@ -0,0 +1,34 @@
+        $(".forms").on("submit", function (e) {
+
+          e.preventDefault();
+
+          console.log("Fetch sendo chamado")
+
+          const id = $(this).data("doc");
+          const endpoint = `/edit/${id}`;
+
+          const title = $(this).find("input[name='title']").val();
+          const conteudo = $(this).find("textarea[name='conteudo']").val();
+
+          const data = {
+          title: title,
+          conteudo: conteudo
+          };
+          
+          fetch(endpoint, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+          })
+            .then((res) => {
+              if (!res.ok) throw new Error("Erro ao Atualizar");
+              return res.json();
+            })
+            .then((result) => {
+            console.log("Atualizado com sucesso");
+            window.location.href = `/blog/${result.post.id}`; 
+          })
+            .catch((e) => console.error("Erro ao atualizar:", e));
+        });
